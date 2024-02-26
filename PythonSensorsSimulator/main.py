dfa0a6640ede738f5fc21c46da09d55036265a6c
Andrea Barutta <andreabarutta123@gmail.com>
2024-02-26 10:55:01 +0100

    merged

--- PythonSensorsSimulator/main.py
+++ PythonSensorsSimulator/main.py
@@ -9,7 +9,7 @@ KAFKA_HOST = os.environ.get("KAFKA_HOST", "kafka")
 KAFKA_PORT = os.environ.get("KAFKA_PORT", "9092")
 # Uso generale di una interfaccia Writer al fine di poter implementare quante politiche diverse di writing si vuole,
 # senza dover cambiare nulla sul resto del codice.
-"""
+
 temp_writers = CompositeWriter().add_kafkaConfluent_writer("temperature", KAFKA_HOST, KAFKA_PORT)
 umd_writers = CompositeWriter().add_kafkaConfluent_writer("umidity", KAFKA_HOST, KAFKA_PORT)
 chS_writers = CompositeWriter().add_kafkaConfluent_writer("chargingStation", KAFKA_HOST, KAFKA_PORT)
@@ -18,7 +18,7 @@ waPr_writers = CompositeWriter().add_kafkaConfluent_writer("waterPresence", KAFK
 """
 from Model.Writers.StdoutWriter import StdoutWriter
 stdout = StdoutWriter()
-
+"""
 symExecAggregator = SimulatorExecutorFactory()
 """
 symExecAggregator \
@@ -36,15 +36,15 @@ symExecAggregator \
 .run()"""
 
 symExecAggregator \
-    .add_simulator(SensorFactory.create_temperature_sensor(stdout, 45.398214, 11.851271, "Arcella", 0.01)) \
-    .add_simulator(SensorFactory.create_temperature_sensor(stdout, 45.388214, 11.691271, "Murelle", 0.01)) \
-    .add_simulator(SensorFactory.create_temperature_sensor(stdout, 45.348214, 11.751271, "Montegrotto", 0.01)) \
-    .add_simulator(SensorFactory.create_temperature_sensor(stdout, 45.368214, 11.951271, "Montegrotto", 0.01)) \
-    .add_simulator(SensorFactory.create_humidity_sensor(stdout, 45.301214, 11.789271, "Arcella", 0.01)) \
-    .add_simulator(SensorFactory.create_humidity_sensor(stdout, 45.291214, 11.787271, "Montegrotto", 0.01)) \
-    .add_simulator(SensorFactory.create_charging_station_sensor(stdout, 45.39214, 11.859271, "Arcella", 0.01)) \
-    .add_simulator(SensorFactory.create_charging_station_sensor(stdout, 45.40214, 11.959271, "Montegrotto", 0.01)) \
-    .add_simulator(SensorFactory.create_ecological_island_sensor(stdout, 45.331214, 11.8901271, "Montegrotto", 0.01)) \
-    .add_simulator(SensorFactory.create_ecological_island_sensor(stdout, 45.291214, 11.901271, "Murelle", 0.01))  \
-    .add_simulator(SensorFactory.create_water_presence_sensor(stdout, 45.591214, 11.879001271,"Murelle", 0.01)) \
+    .add_simulator(SensorFactory.create_temperature_sensor(temp_writers, 45.398214, 11.851271, "Arcella", 0.01)) \
+    .add_simulator(SensorFactory.create_temperature_sensor(temp_writers, 45.388214, 11.691271, "Murelle", 0.01)) \
+    .add_simulator(SensorFactory.create_temperature_sensor(temp_writers, 45.348214, 11.751271, "Montegrotto", 0.01)) \
+    .add_simulator(SensorFactory.create_temperature_sensor(temp_writers, 45.368214, 11.951271, "Montegrotto", 0.01)) \
+    .add_simulator(SensorFactory.create_humidity_sensor(umd_writers, 45.301214, 11.789271, "Arcella", 0.01)) \
+    .add_simulator(SensorFactory.create_humidity_sensor(umd_writers, 45.291214, 11.787271, "Montegrotto", 0.01)) \
+    .add_simulator(SensorFactory.create_charging_station_sensor(chS_writers, 45.39214, 11.859271, "Arcella", 0.01)) \
+    .add_simulator(SensorFactory.create_charging_station_sensor(chS_writers, 45.40214, 11.959271, "Montegrotto", 0.01)) \
+    .add_simulator(SensorFactory.create_ecological_island_sensor(ecoIs_writers, 45.331214, 11.8901271, "Montegrotto", 0.01)) \
+    .add_simulator(SensorFactory.create_ecological_island_sensor(ecoIs_writers, 45.291214, 11.901271, "Murelle", 0.01))  \
+.add_simulator(SensorFactory.create_water_presence_sensor(waPr_writers, 45.591214, 11.879001271,"Murelle", 0.01)) \
 .run()
