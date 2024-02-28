2d0fe175b9e3e38e5c3a4bd92160ea1b34ff988f
Andrea Barutta <andreabarutta123@gmail.com>
2024-02-28 22:43:56 +0100

    grfana variables correction

--- FaustProcessing/HealthStateModel/HealthCalculator.py
+++ FaustProcessing/HealthStateModel/HealthCalculator.py
@@ -1,6 +1,8 @@
 from .ListaMisurazioni import ListaMisurazioni
 from .MisurazioneSalute import MisurazioneSalute
 from .Incrementers.TemperatureIncrementer import TemperatureIncrementer
+from .Incrementers.HumidityIncrementer import HumidityIncrementer
+
 from  .Writers.Writer import Writer
 from datetime import datetime
 import time
@@ -8,13 +10,15 @@ import threading
 
 class HealthCalculator():
 
-    def __init__(self, writer: Writer, temperature_measure_type_naming="temperature", healthScore_measure_type_naming="PunteggioSalute", frequency=10):
+    def __init__(self, writer: Writer, temperature_measure_type_naming:str="temperature",humidity_measure_type_naming:str="humidity", healthScore_measure_type_naming:str="PunteggioSalute", frequency:int=10):
         self.__listaMisurazioni = ListaMisurazioni()
         self.__tmpInc = TemperatureIncrementer()
+        self.__umdInc = HumidityIncrementer()
         self.__writers = writer
         self.__is_running = True
         self.__frequency = frequency
         self.__temperature_measure_type_naming = temperature_measure_type_naming
+        self.__humidity_measure_type_naming = humidity_measure_type_naming
         self.__healthScore_measure_type_naming = healthScore_measure_type_naming
         self.__lock = threading.Lock()  # Lock per sincronizzare l'accesso alla lista
 
@@ -25,10 +29,16 @@ class HealthCalculator():
     def generate_new_health_score(self):
         with self.__lock:  # Acquisizione del lock prima dell'accesso alla lista
             for cella in self.__listaMisurazioni.get_unique_celle():
-                punteggio_cella = self.__tmpInc.get_incrementation(self.__listaMisurazioni.get_list_by_cella_and_type(cella, self.__temperature_measure_type_naming)) 
+                punteggio_cella = self.__calcola_incremento_tmp(cella) + self.__calcola_incremento_umd(cella)
                 self.__writers.write(MisurazioneSalute(datetime.now(), punteggio_cella, self.__healthScore_measure_type_naming, cella))
             self.__listaMisurazioni.clear_list()
+
+    def __calcola_incremento_tmp(self,cella:str):
+        return self.__tmpInc.get_incrementation(self.__listaMisurazioni.get_list_by_cella_and_type(cella, self.__temperature_measure_type_naming))
     
+    def __calcola_incremento_umd(self,cella:str):
+        return self.__umdInc.get_incrementation(self.__listaMisurazioni.get_list_by_cella_and_type(cella, self.__humidity_measure_type_naming))
+
     def run(self) -> None:
         while self.__is_running:
             self.generate_new_health_score()
--- FaustProcessing/HealthStateModel/Incrementers/HumidityIncrementer.py
+++ FaustProcessing/HealthStateModel/Incrementers/HumidityIncrementer.py
@@ -1,14 +1,24 @@
 from .Incrementer import Incrementer
+from typing import List
+from ..Misurazione import Misurazione
 
 class HumidityIncrementer(Incrementer):
-    def __init__(self, upper_health_soglia=70, under_health_soglia=30):
+    def __init__(self, upper_health_soglia: int =70, under_health_soglia:int=30,umidity_type_naming : str ="humidity"):
         self.__upper_health_soglia = upper_health_soglia
         self.__under_health_soglia = under_health_soglia
+        self.__umidity_type_naming =umidity_type_naming
     
-    def get_incrementation(self, dato): #da definire
-        if self.__under_health_soglia <= dato <= self.__upper_health_soglia:
-            return 0
-        elif dato > self.__upper_health_soglia:
-            return dato - self.__upper_health_soglia
-        else:
-            return self.__under_health_soglia - dato
+    def get_incrementation(self, misurazioni: List[Misurazione]):
+        incremento_totale = 0
+        num_misurazioni = len(misurazioni)
+        for misurazione in misurazioni:
+            # Controlla che il tipo di misurazione sia 'umidità'
+            if misurazione.get_type() ==  self.__umidity_type_naming:
+                dato = float(misurazione.get_value())
+                if self.__under_health_soglia <= dato <= self.__upper_health_soglia:
+                    incremento_totale += 0
+                elif dato > self.__upper_health_soglia:
+                    incremento_totale += dato - self.__upper_health_soglia
+                else:
+                    incremento_totale += self.__under_health_soglia - dato
+        return int(incremento_totale / num_misurazioni) if num_misurazioni != 0 else 0
