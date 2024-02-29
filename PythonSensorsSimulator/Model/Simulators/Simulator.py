b8bf1276c10303cd92f14a3cc39f883bdcc4554a
Andrea Barutta <andreabarutta123@gmail.com>
2024-02-29 16:07:11 +0100

    back

--- PythonSensorsSimulator/Model/Simulators/Simulator.py
+++ PythonSensorsSimulator/Model/Simulators/Simulator.py
@@ -2,12 +2,8 @@ from abc import ABC, abstractmethod
 from datetime import datetime
 from .Misurazione import Misurazione
 from .Coordinate import Coordinate
-import random
-from datetime import timedelta
-class Simulator(ABC): 
-    __start_date = datetime(2024, 1, 1)  
-    __end_date = datetime(2024, 12, 31)  
 
+class Simulator(ABC): 
     def __init__(self,  latitude: float, longitude: float, cella: str, sensor_id: str, misurazione_iniziale = 0, type =""):
         self.__ID_sensor = sensor_id
         self.__cella_sensore = cella
@@ -15,23 +11,18 @@ class Simulator(ABC):
         self._misurazione = misurazione_iniziale
         self.__type = type
 
-    #TEMPLATE METHOD
+    #TEMPLATEMETHOD
     def simulate(self) -> None: 
-        self._generate_measure()
-        while not self._filter():
-            self._generate_measure()  # First template step
-        return Misurazione(self._random_datetime(), self._misurazione, self.__type, self.__coordinate, self.__ID_sensor, self.__cella_sensore)
+            self._generate_measure()
+            while not self._filter():
+                self._generate_measure() #First template step
+            return Misurazione(datetime.now(), self._misurazione , self.__type, self.__coordinate, self.__ID_sensor,self.__cella_sensore)
+
 
     @abstractmethod
     def _generate_measure(self) -> None:
         pass
-
-    # Template empty method to override
+    
+    #Template empty method to override
     def _filter(self) -> bool:
         return True
-
-    def _random_datetime(self) -> datetime:
-        delta = Simulator.__end_date - Simulator.__start_date
-        random_seconds = random.randint(0, delta.total_seconds())
-        random_timedelta = timedelta(seconds=random_seconds)
-        return Simulator.__start_date + random_timedelta
