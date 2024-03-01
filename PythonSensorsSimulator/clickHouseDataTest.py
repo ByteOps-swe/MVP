2f239fbc0189bb3d653d3e19969e73fe0e9d1e38
Andrea Barutta <andreabarutta123@gmail.com>
2024-03-01 16:21:45 +0100

    oK

--- PythonSensorsSimulator/clickHouseDataTest.py
+++ PythonSensorsSimulator/clickHouseDataTest.py
@@ -6,10 +6,11 @@ from .Model.Writers.KafkaWriter import KafkaWriter
 from .Model.Writers.kafkaAdapter.KafkaConfluentAdapter import KafkaConfluentAdapter
 from .Model.AdapterMisurazione import AdapterMisurazione
 import clickhouse_connect
+import time
 KAFKA_HOST = os.environ.get("KAFKA_HOST", "kafka")
 KAFKA_PORT = os.environ.get("KAFKA_PORT", "9092")
 
-test_topic = "test_topic"
+test_topic = "temperature"
 
 
 @pytest.fixture(scope='module')
@@ -28,11 +29,13 @@ async def test_1_misurazione(clickhouse_client):
         # print(result.result_rows[0][3])
         adapter_kafka = KafkaConfluentAdapter(test_topic, KAFKA_HOST, KAFKA_PORT)
         kafka_writer = KafkaWriter(adapter_kafka)
-        misurazione = AdapterMisurazione(Misurazione('2024-02-28 10:20:37.206573', 17, "Temperature", Coordinate(45.39214, 11.859271), "Tmp1", "Arcella1"))
+        misurazione = AdapterMisurazione(Misurazione('2022-02-28 10:20:37.206573', 4001, "Temperature", Coordinate(45.39214, 11.859271), "Tmp1", "Arcella1"))
         kafka_writer.write(misurazione) 
         kafka_writer.flush_kafka_producer()
-        result = clickhouse_client.query('SELECT * FROM innovacity.temperatures where value =17 LIMIT 1')
-        print(result.result_rows[0][3])
-        assert float(result.result_rows[0][3]) == 17
+        time.sleep(10)
+        result = clickhouse_client.query('SELECT * FROM innovacity.temperatures where value =4001 LIMIT 1')
+        print(result.result_rows)
+        assert result.result_rows
+        assert float(result.result_rows[0][3]) == 4001
     except Exception as e:
         pytest.fail(f"Failed to connect to ClickHouse database: {e}")
