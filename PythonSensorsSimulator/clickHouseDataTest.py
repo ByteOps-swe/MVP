e3f1a6a3ec1602fcf2fc997a9afd5106881ff8d8
Lisien <lisienskenderi@hotmail.com>
2024-03-04 10:01:43 +0100

    Aggiutno il test di 1000 dati, da testare questo test

--- PythonSensorsSimulator/clickHouseDataTest.py
+++ PythonSensorsSimulator/clickHouseDataTest.py
@@ -39,3 +39,40 @@ async def test_1_misurazione(clickhouse_client):
         assert float(result.result_rows[0][3]) == 4001
     except Exception as e:
         pytest.fail(f"Failed to connect to ClickHouse database: {e}")
+        
+
+
+@pytest.mark.asyncio
+async def test_2_misurazione(clickhouse_client):
+    try:
+        adapter_kafka = KafkaConfluentAdapter(test_topic, KAFKA_HOST, KAFKA_PORT)
+        kafka_writer = KafkaWriter(adapter_kafka)
+        num_messages = 1000  # Number of messages to send
+        
+        # Send data to Kafka
+        for i in range(num_messages):
+            misurazione = AdapterMisurazione(Misurazione(f'2022-02-28 10:20:37.{i}', 5001 + i, "Temperature", Coordinate(45.39214, 11.859271), "Tmp1", "Arcella1"))
+            kafka_writer.write(misurazione) 
+
+        # Track initial Kafka offsets
+        initial_offsets = adapter_kafka.consumer.position()
+
+        # Poll Kafka consumer until all messages are consumed
+        timeout = 60  # Maximum wait time in seconds
+        start_time = time.time()
+        while True:
+            # Check if all messages have been consumed
+            current_offsets = adapter_kafka.consumer.position()
+            if all(current_offsets[topic_partition] >= initial_offsets[topic_partition] + num_messages for topic_partition in current_offsets):
+                break  # Exit loop if all messages have been consumed
+            # Check if timeout has been reached
+            if time.time() - start_time > timeout:
+                raise TimeoutError("Timed out waiting for all messages to be consumed by ClickHouse")
+            time.sleep(1)  # Polling interval
+
+        # Query ClickHouse to check if all data has been inserted
+        result = clickhouse_client.query(f'SELECT COUNT(*) FROM innovacity.temperatures WHERE timestamp >= \'2022-02-28 10:20:37.0\' AND timestamp < \'2022-02-28 10:20:38.{num_messages}\'')
+        assert result.result_rows[0][0] == num_messages
+
+    except Exception as e:
+        pytest.fail(f"Failed to send and consume data: {e}")
