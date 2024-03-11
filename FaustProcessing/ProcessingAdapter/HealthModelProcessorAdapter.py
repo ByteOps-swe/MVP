0c19c8f2566a83a5280eeaed7acf17ffee33f3ad
Shinji76 <francesco.pozza.3@gmail.com>
2024-03-11 18:20:07 +0100

    bug fix formattazione e manutenibilità

--- FaustProcessing/ProcessingAdapter/HealthModelProcessorAdapter.py
+++ FaustProcessing/ProcessingAdapter/HealthModelProcessorAdapter.py
@@ -6,11 +6,11 @@ class HealthModelProcessorAdapter(Processor):
     def __init__(self, healthCalculator:HealthCalculator):
         self.healthCalculator = healthCalculator
 
-    async def process(self, measurement:FaustMeasurement):
-        self.healthCalculator.add_misurazione(measurement.timestamp,
-                                                measurement.value,
-                                                measurement.type,
-                                                measurement.latitude,
-                                                measurement.longitude,
-                                                measurement.ID_sensore,
-                                                measurement.cella)
+    async def process(self, misurazione:FaustMeasurement):
+        self.healthCalculator.add_misurazione(misurazione.timestamp,
+                                                misurazione.value,
+                                                misurazione.type,
+                                                misurazione.latitude,
+                                                misurazione.longitude,
+                                                misurazione.ID_sensore,
+                                                misurazione.cella)
