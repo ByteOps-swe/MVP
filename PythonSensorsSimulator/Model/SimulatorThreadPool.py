db97c9501ec1a803efb413559c32fff131dc12f8
Andrea Barutta <andreabarutta123@gmail.com>
2024-03-13 16:34:26 +0100

    correction

--- PythonSensorsSimulator/Model/SimulatorThreadPool.py
+++ PythonSensorsSimulator/Model/SimulatorThreadPool.py
@@ -11,16 +11,17 @@ class SimulatorThreadPool:
         self.__thread_pool_adapter = thread_pool_adapter
 
     def run_all(self):
-        self.__thread_pool_adapter.map(self.__start_simulator, self.__simulators)
+        self.__thread_pool_adapter.map(SimulatorThreadPool.__start_simulator, self.__simulators)
 
     def stop_all(self):
-        self.__thread_pool_adapter.map(self.__stop_simulator, self.__simulators)
+        self.__thread_pool_adapter.map(SimulatorThreadPool.__stop_simulator, self.__simulators)
 
     def append_simulator(self, simulator: ComponentSimulatorThread):
         self.__simulators.append(simulator)
 
-    def __start_simulator(self, simulator: ComponentSimulatorThread):
+    @staticmethod
+    def __start_simulator(simulator: ComponentSimulatorThread):
         simulator.task()
-
-    def __stop_simulator(self, simulator: ComponentSimulatorThread):
+    @staticmethod
+    def __stop_simulator(simulator: ComponentSimulatorThread):
         simulator.stop()
