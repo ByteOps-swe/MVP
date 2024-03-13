866a8fe96dc3682f5883ba56616ebc8cda89df5f
Andrea Barutta <andreabarutta123@gmail.com>
2024-03-13 11:53:42 +0100

    correction

---
+++ FaustProcessing/HealthStateModel/HealthProcessorBuffer.py
@@ -0,0 +1,21 @@
+from abc import ABC
+import threading
+from .ListaMisurazioni import ListaMisurazioni
+
+
+#Pattern Strategy
+class HealthProcessorBuffer(ABC):
+
+    def __init__(self):
+        self.__listaMisurazioni = ListaMisurazioni()
+        self.__lock = threading.Lock()
+
+    def add_misurazione(self, timestamp, value, type_, latitude, longitude, ID_sensore, cella):
+        with self.__lock:
+            self.__listaMisurazioni.add_misurazione(timestamp, value, type_, latitude, longitude, ID_sensore, cella)
+
+    def get_lista_misurazioni(self):
+        return self.__listaMisurazioni
+    
+    def clear_list(self):
+        self.__listaMisurazioni.clear_list()
